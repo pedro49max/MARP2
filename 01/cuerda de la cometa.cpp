786d58ae859fa04f689f05843c05651006368e9b
pedro49max <pedrotob@ucm.es>
2025-01-21 18:03:10 +0100

    Update cuerda de la cometa.cpp

--- 01/cuerda de la cometa.cpp
+++ 01/cuerda de la cometa.cpp
@@ -3,11 +3,22 @@
 #include<vector>
 using namespace std;
 
+void solve() {
+	int n, L;
+	while (cin >> n >> L) {
+		vector<int> leghts(n);
+		vector<int> costs(n);
+		for (int i = 0; i < n; i++)
+			cin >> leghts[i] >> costs[i];
+	}
+}
+
 
 int main() {
 #ifndef DOMJUDGE
 	std::ifstream in("in.txt");
 	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
 #endif
+	solve();
 	return 0;
 }
