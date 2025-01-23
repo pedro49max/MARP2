04af9448c8e4538f041150b7a112cd08f029f886
pedro49max <pedrotob@ucm.es>
2025-01-23 10:38:33 +0100

    02 finished

--- 02/constrirDianas.cpp
+++ 02/constrirDianas.cpp
@@ -13,46 +13,49 @@ void solve() {
 		for (int i = 0; i < S; i++) {
 			cin >> secciones[i];
 		}
-		vector<long long> dp_ways(O + 1, 0); // número de formas de conseguir cada puntuacion
 		vector<int> dp_min_dards(O + 1, INT_MAX); // mínimo número de dardos para cada puntuacion
-        vector<vector<int>> puntuaciones(O + 1);//queue con las secciones del minimo de dardos para la puntuacion
+        vector<int> backtrack(O + 1, -1); // Para rastrear qué puntuación llevó al estado óptimo
 
 
         // Inicializar para longitud 0
-        dp_ways[0] = 1;
         dp_min_dards[0] = 0;
 
-        for (int i = S-1; i >=0; i--) {
-            for (int j = secciones[i]; j <= O && j - secciones[i] >= 0; j++) {
-                // Actualizar número de formas
-                dp_ways[j] += dp_ways[j - secciones[i]]; // Al número de formas de llegar a j, se le suman el numero de formas de llegar a j - leghts[i]
 
-                // Actualizar mínimo número de dardos
-                if (dp_min_dards[j - secciones[i]] != INT_MAX) {
-                    if (dp_min_dards[j - secciones[i]] + 1 < dp_min_dards[j]) {
-                        dp_min_dards[j] =dp_min_dards[j - secciones[i]] + 1; // se guarda el valor del numero minimo de dardos necesarios para llegar a j
-                        puntuaciones[j] = puntuaciones[j - secciones[i]]; puntuaciones[j].push_back(secciones[i]);
-                    }
-                    else if(dp_min_dards[j - secciones[i]] + 1 == dp_min_dards[j]){
-                        int idx = 0;
-                        while (idx < puntuaciones[j - secciones[i]].size() && puntuaciones[j - secciones[i]][idx] == puntuaciones[j][idx])
-                            idx++;
-                        if (puntuaciones[j - secciones[i]][idx] > puntuaciones[j][idx]) {
-                            puntuaciones[j] = puntuaciones[j - secciones[i]]; puntuaciones[j].push_back(secciones[i]);
-                        }
+        // Programación dinámica
+        for (int i = 0; i < S; ++i) {
+            for (int j = secciones[i]; j <= O; ++j) {
+                if (dp_min_dards[j - secciones[i]] != INT_MAX && dp_min_dards[j - secciones[i]] + 1 <= dp_min_dards[j]) {
+                    // Actualizar si encontramos una solución más corta o igual
+                    if (dp_min_dards[j - secciones[i]] + 1 < dp_min_dards[j] ||
+                        (backtrack[j - secciones[i]] != -1 && secciones[i] > secciones[backtrack[j]])) {
+                        dp_min_dards[j] = dp_min_dards[j - secciones[i]] + 1;
+                        backtrack[j] = i; // Guardamos el índice de la puntuación usada
                     }
                 }
             }
         }
-        if (dp_ways[O] > 0) {
-            cout << dp_min_dards[O] << ":";
-            for (int i = 0; i < puntuaciones[O].size(); i++) {
-                cout << " " << puntuaciones[O][i]; 
-            }
-            cout << endl;
+        // Construir resultado
+        if (dp_min_dards[O] == INT_MAX) {
+            cout << "Imposible" << endl;
         }
         else {
-            cout << "Imposible" << endl;
+            cout << dp_min_dards[O] << ":";
+            vector<int> resultado;
+            int current = O;
+
+            // Rastreamos las puntuaciones utilizadas
+            while (current > 0) {
+                resultado.push_back(secciones[backtrack[current]]);
+                current -= secciones[backtrack[current]];
+            }
+
+            // Ordenar las puntuaciones de mayor a menor
+            sort(resultado.rbegin(), resultado.rend());
+
+            for (int puntuacion : resultado) {
+                cout << " " << puntuacion;
+            }
+            cout << endl;
         }
 	}
 
