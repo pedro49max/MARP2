1a7bf3fc9df911a81f482e67bdf26eb60354b284
pedro49max <pedrotob@ucm.es>
2025-01-27 14:13:01 +0100

    3 y 4 init

---
+++ 03/porDondeSubir.cpp
@@ -0,0 +1,20 @@
+#include<fstream>
+#include<iostream>
+#include<vector>
+#include<queue>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+void solve() {
+
+}
+
+int main() {
+#ifndef DOMJUDGE
+	std::ifstream in("in.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+	solve();
+	return 0;
+}
