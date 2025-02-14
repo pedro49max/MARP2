d82625802fbe011f7eab9bb09a7f98426858c956
pedro49max <pedrotob@ucm.es>
2025-02-14 17:31:57 +0100

    11

--- 11/PocasMonedas.cpp
+++ 11/PocasMonedas.cpp
@@ -11,12 +11,37 @@ using namespace std;
 void solve() {
     int N;
     while (cin >> N && N != 0) {
-        vector<int>cubos(N);
+        vector<int>valores(N);
         for (int i = 0; i < N; i++) {
-            cin >> cubos[i];
+            cin >> valores[i];
         }
-        vector<vector<int>>dp(N, vector<int>(N, -1));
-        cout << resuelve(cubos, dp, 0, N - 1, 0) << endl;
+        vector<int>cantidades(N);
+        for (int i = 0; i < N; i++) {
+            cin >> cantidades[i];
+        }
+        int precio;
+        cin >> precio;
+         vector<int> dp(precio + 1, INT_MAX);
+        dp[0] = 0;
+
+        for (int i = 0; i < N; ++i) {
+            vector<int> nueva_dp = dp;
+            for (int j = 0; j <= precio; ++j) {
+                if (dp[j] != INT_MAX) {
+                    for (int k = 1; k <= cantidades[i] && j + k * valores[i] <= precio; ++k) {
+                        nueva_dp[j + k * valores[i]] = min(nueva_dp[j + k * valores[i]], dp[j] + k);
+                    }
+                }
+            }
+            dp = nueva_dp;
+        }
+
+        if (dp[precio] == INT_MAX) {
+            cout << "NO" << endl;
+        }
+        else {
+            cout << "SI " << dp[precio] << endl;
+        }
     }
 }
 
