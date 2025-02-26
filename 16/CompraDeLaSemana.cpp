4939464d6342193a72f7215f6461bf52066eabf7
pedro49max <pedrotob@ucm.es>
2025-02-26 15:48:40 +0100

    16

--- 16/CompraDeLaSemana.cpp
+++ 16/CompraDeLaSemana.cpp
@@ -1,63 +1,101 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include<queue>
+#include<climits>
 #include <fstream>
 
 using namespace std;
 
-vector<vector<int>> MatrizAhorro(const vector<vector<int>>& superProducto) {//O(S*2*P) -> O(S*P)
-    vector<vector<int>> ahorro(superProducto.size(), vector<int>(superProducto[0].size()));
-    for (int producto = 0; producto < superProducto[0].size(); producto++) {
-        int maximo= 0;
-        for (int tienda = 0; tienda < superProducto.size(); tienda++) {
-            if (superProducto[tienda][producto] > maximo)
-                maximo = superProducto[tienda][producto];
-        }
-        for (int tienda = 0; tienda < superProducto.size(); tienda++) {
-            ahorro[tienda][producto] = superProducto[tienda][producto] - maximo;
-        }
-    }
-    return ahorro;
+struct Nodo {
+    int prodID;
+    int costeAcomulado;
+    int costeOptimista;
+    int costePesimista;
+    vector<int> counterSuper;
+};
+
+bool operator>(Nodo a, Nodo b) {
+    return a.costeOptimista > b.costeOptimista;
 }
 
-void prodMin(const vector<vector<int>>& superProducto, const vector<bool>& comprado, int &super, int &prod) { //O(S*P)
-    int mini = -1;
-    for(int j = 0; j < superProducto.size(); j++)
-        for (int i = 0; i < superProducto[j].size(); i++) {
-            if (!comprado[i] && (mini == -1 || superProducto[j][i] < mini)) {
-                super = j;
-                prod = i;
-                mini = superProducto[j][i];
+typedef priority_queue<Nodo, vector<Nodo>, greater<Nodo>> pq;
+
+
+// Cálculo de la cota optimista con una heurística voraz
+int cota_optimista(const int &producto, const int &supers, const vector<vector<int>> &superProducto, const int producIdx) {//O(S*P)
+    int costeEstimado = 0;
+    vector<int> preciosMinimos(producto, INT_MAX);
+
+    for (int i = 0; i < supers; i++) {
+        for (int j = producIdx; j < producto; j++) {
+            preciosMinimos[j] = min(preciosMinimos[j], superProducto[i][j]);
+        }
+    }
+
+    for (int j = producIdx; j < producto; j++) {
+         costeEstimado += preciosMinimos[j];
+    }
+
+    return costeEstimado;
+}
+
+int cota_pesimista(const int& producto, const int& supers, const vector<vector<int>>& superProducto, const int producIdx) {//O(S*P)
+    int costeEstimado = 0;
+    vector<int> preciosMaximos(producto, INT_MIN);
+
+    for (int i = 0; i < supers; i++) {
+        for (int j = producIdx; j < producto; j++) {
+            preciosMaximos[j] = max(preciosMaximos[j], superProducto[i][j]);
+        }
+    }
+
+    for (int j = producIdx; j < producto; j++) {
+         costeEstimado += preciosMaximos[j];
+    }
+
+    return costeEstimado;
+}
+
+int contarBits(int n) {
+    int counter = 0;
+    while (n) {
+        counter += (n & 1); // Suma 1 si el bit menos significativo es 1
+        n >>= 1;          // Desplaza bits a la derecha
+    }
+    return counter;
+}
+
+
+// Backtracking con poda optimista-pesimista
+void iterativo(const int &productos, const int &supers, const vector<vector<int>>& superProducto, vector<bool>& carrito, int &costeMejor) {
+    pq nodos;
+    int cotaOptimista = cota_optimista(productos, supers, superProducto, 0), cotaPesimista = cota_pesimista(productos, supers, superProducto, 0);
+    nodos.push({ 0,0, cotaOptimista, cotaPesimista, vector<int>(supers, 0)});
+    while (!nodos.empty()) {
+        Nodo a = nodos.top(); nodos.pop();
+        if (a.prodID == productos) {
+            costeMejor = min(a.costeAcomulado, costeMejor);
+            break;
+        }
+        else if (a.costeOptimista <= costeMejor) {
+            int prodID = a.prodID;
+            for (int i = 0; i < supers; i++) {
+                if (a.counterSuper[i] < 3) {          
+                    vector<int> counterSuper = a.counterSuper; counterSuper[i]++;
+                    int costeAcomulado = a.costeAcomulado + superProducto[i][prodID];
+                    int costeOptimista = costeAcomulado + cota_optimista(productos, supers, superProducto, prodID + 1);
+                    int costePesimista = costeAcomulado + cota_pesimista(productos, supers, superProducto, prodID + 1);
+                    if (cotaOptimista <= costeMejor) {
+                        nodos.push({ prodID + 1, costeAcomulado, costeOptimista, costePesimista, counterSuper });
+                        if (costePesimista < costeMejor)
+                            costeMejor = costePesimista;
+                    }
+                }
             }
         }
-}
-bool faltanProductos(const vector<bool>& comprado) { //O(P)
-    bool faltaAlgo = false;
-    for (int i = 0; i < comprado.size() && !faltaAlgo; i++) {
-        if (!comprado[i])
-            faltaAlgo = true;
+        
     }
-    return faltaAlgo;
-}
-
-void podadora(const vector<vector<int>> &superProducto, vector<vector<int>> &ahorro, vector<bool> &comprado, int &minimo) {
-    int costeAcomulado = 0;
-    vector<int> comprasPorTienda(superProducto.size(), 0);
-    while (faltanProductos(comprado)) {
-        int super = -1, prod = -1;
-        prodMin(ahorro, comprado, super, prod);
-        if (comprasPorTienda[super] < 3) {
-            comprado[prod] = true;
-            comprasPorTienda[super]++;
-            costeAcomulado += superProducto[super][prod];
-            cout << "I take the shop " << super << " and the product " << prod << " and now I have a sum of " << costeAcomulado << endl;
-        }
-        else {
-            for(int i = 0; i < ahorro[super].size(); i++)
-                ahorro[super][i] = 1;
-        }
-    }
-    minimo = costeAcomulado;
 }
 
 
@@ -74,9 +112,8 @@ void solve() {
             }
         }
         vector<bool> comprado(productos, false);
-        vector<vector<int>> ahorro = MatrizAhorro(superProducto);
         int minimo = INT_MAX;
-        podadora(superProducto,ahorro, comprado, minimo);
+        iterativo(productos, supers, superProducto, comprado, minimo);
 
         cout << minimo << endl;
     }
