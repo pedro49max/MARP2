8695c4ab70db104cdc35d160ead15694417a0a3e
pedro49max <pedrotob@ucm.es>
2025-04-11 17:21:55 +0200

    Update porDondeSubir.cpp

--- 03/porDondeSubir.cpp
+++ 03/porDondeSubir.cpp
@@ -6,6 +6,48 @@
 #include <climits>
 using namespace std;
 
+/*
+RECURSION
+Parametros:
+    i es el index de la seccion
+    j es la puntuacion a la que se quiere llegar con esa seccion
+    dp_min_dards es el vector que guarda el numero minimo de secciones para llegar a la puntuacion j
+    backtrack es el vector que guarda la ultima seccion utilizada para legar a la puntuacion j
+Rangos:
+    las secciones van de la 0 a la S
+    los vectores solucion van del 0 a O+1
+Casos base: 
+    if (dp_min_dards[j - secciones[i]] == INT_MAX || dp_min_dards[j - secciones[i]] + 1 == dp_min_dards[j]) break;
+Casos recursivos:
+    if (dp_min_dards[j - secciones[i]] != INT_MAX && dp_min_dards[j - secciones[i]] + 1 <= dp_min_dards[j])
+        calculate vector values at j
+        j++
+
+*/
+/*
+IMPLEMENACION
+Iterativo: 
+    Como relleno el vector:
+        Primero itero por las secciones, de la 1 a la n, y despues con cada seccion voy iterndo desde
+        la puntuacion de la seccion hasta la puntuacion objectivo,  asi que voy rellenando de forma ascendente,
+        para asi poder tener el numero minimo de secciones para llegar a j, y añadir las secciones a backtrack
+*/
+/*
+COSTE DE RELLENADO DE LA MATRIZ:
+Tiempo: O(S*O)
+Espacio: O(O)
+    El espacio ya esta reducido a 1 dimension por lo que no se podría reducir más ya que necesitamos los valores de diversas puntciones
+*/
+/*
+RECONSTRUCCION:
+Algoritmo:
+    Dado que ya tengo la resconstruccion en backtrack, el algoritmo consiste en iterar por backtrack, para ir sacando las secciones 
+    que se han ido utilizando, pues al tener gurdada backrack la ultima seccion, puedes iterar restamdo la puntuacion de esa seccion
+Coste: 
+    Tiempo:O(O)
+    Espacio: O(S)
+*/
+
 void solve() {
 	int n;
 	while (cin >> n) {
